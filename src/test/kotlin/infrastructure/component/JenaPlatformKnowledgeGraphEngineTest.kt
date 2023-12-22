34786522e430c72a9e9a90a71fdbdad4c3bed459
Andrea Giulianelli <andrygiulio@gmail.com>
2023-12-22 19:07:07 +0100

    test: add platform knowledge graph engine tests

---
+++ src/test/kotlin/infrastructure/component/JenaPlatformKnowledgeGraphEngineTest.kt
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package infrastructure.component
+
+import TestingUtils.readResourceFile
+import application.component.PlatformKnowledgeGraphEngine
+import application.presenter.dtd.DigitalTwinDescriptorDeserialization.toDTD
+import application.service.EcosystemRegistryService
+import entity.digitaltwin.DigitalTwinURI
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class JenaPlatformKnowledgeGraphEngineTest : StringSpec({
+    val testPort = 4000
+    val dtUri = DigitalTwinURI("https://example.com/dt")
+    val selectQuery = """
+            SELECT ?subject ?predicate ?object
+            WHERE {
+              ?subject ?predicate ?object
+            }
+            LIMIT 10
+    """.trimIndent()
+    val askQuery = """
+        ASK
+        WHERE {
+          ?subject ?predicate <http://example.com/Person>
+        }
+    """.trimIndent()
+    val constructQuery = """
+        CONSTRUCT {
+          ?subject <http://example.it/is> <http://example.com/Person>;
+        }
+        WHERE {
+          ?subject ?predicate <http://example.com/Person>.
+        }
+        LIMIT 10
+    """.trimIndent()
+    val describeQuery = """
+        DESCRIBE <${dtUri.uri}>
+    """.trimIndent()
+    val sparqlUpdateQuery = """
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+        INSERT DATA {
+          <http://example.org/johndoe> foaf:name "John Doe" .
+          <http://example.org/johndoe> rdf:type foaf:Person .
+        }
+    """.trimIndent()
+
+    fun insertDTD(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngine) {
+        readResourceFile("wotDtd.json")?.toDTD("application/td+json")?.run {
+            platformKnowledgeGraphEngine.mergeDigitalTwinDescriptor(this)
+        }
+    }
+
+    fun insertDTKG(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngine): String =
+        readResourceFile("dtkgWithRelationship.ttl")?.also {
+            platformKnowledgeGraphEngine.mergeDigitalTwinKnowledgeGraphUpdate(dtUri, it)
+        }.orEmpty()
+
+    "it should be possible to merge a Digital Twin Descriptor" {
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(EcosystemRegistryService(testPort))
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph()?.isEmpty() shouldBe true
+        insertDTD(platformKnowledgeGraphEngine)
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph()?.isEmpty() shouldBe false
+    }
+
+    "it should be possible to merge a Digital Twin Knowledge Graph" {
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(EcosystemRegistryService(testPort))
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph()?.isEmpty() shouldBe true
+        val dtkg = insertDTKG(platformKnowledgeGraphEngine)
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph() shouldBe dtkg
+    }
+
+    "it should be possible to delete a registered WoDT Digital Twin" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        ecosystemRegistry.signalRegistration(dtUri)
+        insertDTD(platformKnowledgeGraphEngine)
+        insertDTKG(platformKnowledgeGraphEngine)
+        platformKnowledgeGraphEngine.deleteDigitalTwin(dtUri) shouldBe true
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph()?.isEmpty() shouldBe true
+    }
+
+    "it should not be possible to delete a not-registered Digital Twin" {
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(EcosystemRegistryService(testPort))
+        platformKnowledgeGraphEngine.deleteDigitalTwin(dtUri) shouldBe false
+    }
+
+    "it should be possible to obtain the cached representation of a registered WoDT Digital Twin" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        ecosystemRegistry.signalRegistration(dtUri)
+        insertDTKG(platformKnowledgeGraphEngine)
+        readResourceFile("mappedDtkgWithRelationship.ttl")?.run {
+            platformKnowledgeGraphEngine.currentCachedDigitalTwinKnowledgeGraph(
+                "http://localhost:$testPort/wodt/${dtUri.uri}",
+            ) shouldBe this
+        }
+    }
+
+    "it should be possible to obtain all the registered WoDT Digital Twins that are associated with " +
+        "a specific Physical Asset" {
+            val ecosystemRegistry = EcosystemRegistryService(testPort)
+            val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+            ecosystemRegistry.signalRegistration(dtUri)
+            insertDTD(platformKnowledgeGraphEngine)
+            platformKnowledgeGraphEngine.getDigitalTwinsFromPhysicalAsset("lampPA")
+                .contains(dtUri) shouldBe true
+        }
+
+    "it should be possible to perform SPARQL SELECT Queries using XML content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            selectQuery,
+            "application/sparql-results+xml",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL SELECT Queries using JSON content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            selectQuery,
+            "application/sparql-results+json",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL SELECT Queries using CSV content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            selectQuery,
+            "text/csv",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL SELECT Queries using TSV content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            selectQuery,
+            "text/tab-separated-values",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL ASK Queries using XML content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            askQuery,
+            "application/sparql-results+xml",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL ASK Queries using JSON content-type" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(
+            askQuery,
+            "application/sparql-results+json",
+        ) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL CONSTRUCT Queries" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(constructQuery, null) shouldNotBe null
+    }
+
+    "it should be possible to perform SPARQL DESCRIBE Queries" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(describeQuery, null) shouldNotBe null
+    }
+
+    "it should not be possible to perform SPARQL UPDATE queries" {
+        val ecosystemRegistry = EcosystemRegistryService(testPort)
+        val platformKnowledgeGraphEngine = JenaPlatformKnowledgeGraphEngine(ecosystemRegistry)
+        platformKnowledgeGraphEngine.query(sparqlUpdateQuery, null) shouldBe null
+    }
+})
