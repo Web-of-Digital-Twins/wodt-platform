a88a378f6abc206467663ad87e295b08c2fc2d0d
Andrea Giulianelli <andrygiulio@gmail.com>
2023-12-23 15:09:33 +0100

    feat: implement wodt digital twins platform interface api

---
+++ src/main/kotlin/infrastructure/component/WoDTDigitalTwinsPlatformInterfaceAPI.kt
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package infrastructure.component
+
+import application.component.PlatformKnowledgeGraphEngineReader
+import entity.digitaltwin.DigitalTwinURI
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.request.contentType
+import io.ktor.server.request.header
+import io.ktor.server.request.receiveText
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.send
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+/**
+ * The WoDT Digital Twins Platform Interface API available to handle requests of Consumers.
+ */
+fun Application.wodtDigitalTwinsPlatformInterfaceAPI(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) {
+    routing {
+        getPlatform()
+        getCompletePlatformKnowledgeGraph(platformKnowledgeGraphEngine)
+        getLocalDigitalTwinKnowledgeGraph(platformKnowledgeGraphEngine)
+        queryOnPlatformKnowledgeGraph(platformKnowledgeGraphEngine)
+        observePlatformKnowledgeGraph(platformKnowledgeGraphEngine)
+        getDigitalTwinsFromPhysicalAssetId(platformKnowledgeGraphEngine)
+    }
+}
+
+private fun Route.getPlatform() =
+    get("/") {
+        call.response.headers.append(HttpHeaders.Location, "/wodt")
+        call.respond(HttpStatusCode.SeeOther)
+    }
+
+private fun Route.getCompletePlatformKnowledgeGraph(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) =
+    get("/wodt") {
+        platformKnowledgeGraphEngine.currentPlatformKnowledgeGraph().apply {
+            when (this) {
+                null -> call.respond(HttpStatusCode.NoContent)
+                else -> {
+                    call.response.status(
+                        if (this.isEmpty() || this.isBlank()) { HttpStatusCode.NoContent } else { HttpStatusCode.OK },
+                    )
+                    call.response.headers.append(HttpHeaders.ContentType, "text/turtle")
+                    call.respond(this)
+                }
+            }
+        }
+    }
+
+private fun Route.getLocalDigitalTwinKnowledgeGraph(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) =
+    get("/wodt/{dtUri...}") {
+        call.parameters.getAll("dtUri")?.also { pathParameters ->
+            if (pathParameters.size >= 2) {
+                val dtUri = obtainDigitalTwinUriFromPathParameters(pathParameters)
+                platformKnowledgeGraphEngine.currentCachedDigitalTwinKnowledgeGraph(DigitalTwinURI(dtUri)).apply {
+                    when (this) {
+                        null -> call.respond(HttpStatusCode.NotFound)
+                        else -> {
+                            call.response.status(
+                                if (this.isEmpty() || this.isBlank()) {
+                                    HttpStatusCode.NoContent
+                                } else { HttpStatusCode.OK },
+                            )
+                            call.response.headers.append(HttpHeaders.ContentType, "text/turtle")
+                            call.response.headers.append(HttpHeaders.Link, "<$dtUri>; rel=\"original\"")
+                            call.respond(this)
+                        }
+                    }
+                }
+            } else {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+private fun Route.queryOnPlatformKnowledgeGraph(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) =
+    post("/wodt/sparql") {
+        val acceptedContentType = call.request.header(HttpHeaders.Accept).toString()
+        if (call.request.contentType().toString() == "application/sparql-query") {
+            platformKnowledgeGraphEngine.query(call.receiveText(), acceptedContentType).apply {
+                when (this) {
+                    null -> call.respond(HttpStatusCode.BadRequest)
+                    else -> {
+                        call.response.headers.append(HttpHeaders.ContentType, acceptedContentType)
+                        call.respond(HttpStatusCode.OK, this)
+                    }
+                }
+            }
+        } else {
+            call.respond(HttpStatusCode.BadRequest)
+        }
+    }
+
+private fun Route.observePlatformKnowledgeGraph(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) =
+    webSocket("/wodt") {
+        platformKnowledgeGraphEngine.platformKnowledgeGraphs.collect {
+            send(it)
+        }
+    }
+
+private fun Route.getDigitalTwinsFromPhysicalAssetId(platformKnowledgeGraphEngine: PlatformKnowledgeGraphEngineReader) =
+    get("/wodt/directory") {
+        val paId = call.request.queryParameters["pa"]
+        if (paId != null) {
+            platformKnowledgeGraphEngine.getDigitalTwinsFromPhysicalAsset(paId).apply {
+                call.response.status(if (this.isEmpty()) { HttpStatusCode.NoContent } else { HttpStatusCode.OK })
+                call.response.headers.append(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                call.respond(Json.encodeToString(this.map { it.uri }))
+            }
+        } else {
+            call.respond(HttpStatusCode.BadRequest)
+        }
+    }
